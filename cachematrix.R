c7a074f4235c9f47b955151e26077fae441f2579
cmanzo <carlo.manzo@gmail.com>
2015-09-24 10:25:33 +0200

    file description still missing

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  in_x <- NULL
+  set <- function(y) {
+    x <<- y
+    in_x <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) in_x <<- solve
+  getinv <- function() in_x
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix 'in_x' that is the inverse of 'x'
+  in_x <- x$getinv()
+  if(!is.null(in_x)) {
+    message("getting cached data")
+    return(in_x)
+  }
+  data <- x$get()
+  in_x <- solve(data, ...)
+  x$setinv(in_x)
+  in_x
 }
